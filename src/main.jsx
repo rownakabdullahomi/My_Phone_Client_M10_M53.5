ad773d6b8991700e00d3ffc00b56436b313dbc76
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-25 01:36:51 +0600

    my phone client is done

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/Main.jsx';
+import Phones from './components/Phones.jsx';
+import Phone from './components/Phone.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/phones",
+        element: <Phones />,
+        loader: ()=> fetch("http://localhost:5000/phones")
+      },
+      {
+        path: "/phone/:id",
+        element: <Phone></Phone>,
+        loader: ({params})=> fetch(`http://localhost:5000/phones/${params.id}`),
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
